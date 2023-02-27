c5102e9af0841a4cd0174e613e8840f3f7358506
kms <mindino03@gmail.com>
2023-02-27 23:22:36 +0900

    Add - layout, store listing view
    blade 템플릿 레이아웃 적용
    상점 리스팅 페이지 생성
    추가 퍼블리싱 필요

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">SHM 쇼핑몰</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+</div>
